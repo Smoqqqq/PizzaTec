a1ea918561fa34b5aa522ebf0714d8c829d06cd3
alolilu <33064126+alolilu@users.noreply.github.com>
2021-06-11 14:31:14 +0200

    test

--- modules/navbar.php
+++ modules/navbar.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-md-lg-3">
             <i class="fas fa-bars"></i>
-            <h1>dd</h1>
+            <h1>ddd</h1>
         </div>
         <div class="col-md-lg-5">
         </div>
