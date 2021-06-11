4cc2eb04b8b089e4a931bf9eb2c339615d79a9fc
Le Flem Paul <61330908+Smoqqqq@users.noreply.github.com>
2021-06-11 14:56:27 +0200

    login logic

--- functions/login.php
+++ functions/login.php
@@ -2,8 +2,17 @@
 
 if(isset($_POST['login'])){
     $email = $_POST['email'];
-    $sql = "SELECT * FROM users WHERE email = :email";
+    $password = hash("sha256", $_POST['password'], false);
+    $sql = "SELECT * FROM users WHERE email = :email AND password = :password";
     $query = $dbh->prepare($sql);
     $query->bindParam(":email", $email, PDO::PARAM_STR);
+    $query->bindParam(":password", $password, PDO::PARAM_STR);
     $query->execute();
+    $data = $query->fetchAll(PDO::FETCH_OBJ);
+
+    if(isset($data->email)){
+        echo "success";
+    }else{
+        echo "error";
+    }
 }
