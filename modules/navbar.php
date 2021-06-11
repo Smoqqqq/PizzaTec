b60b96e405bf310c64b992a3465d7c4ec7025929
Le Flem Paul <61330908+Smoqqqq@users.noreply.github.com>
2021-06-11 14:51:59 +0200

    changed

--- modules/navbar.php
+++ modules/navbar.php
@@ -2,7 +2,6 @@
     <div class="row">
         <div class="col-md-lg-3">
             <i class="fas fa-bars"></i>
-            <h1>ddd</h1>
         </div>
         <div class="col-md-lg-5">
         </div>
