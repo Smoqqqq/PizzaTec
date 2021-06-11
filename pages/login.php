a82f0396e694addc12fea8c57c7969287a7a3a34
Le Flem Paul <61330908+Smoqqqq@users.noreply.github.com>
2021-06-11 14:27:04 +0200

    add login inputs

--- pages/login.php
+++ pages/login.php
@@ -1,8 +1,10 @@
 <div class="container">
     <div class="row">
-        <div class="col-sm-6 offset-6">
-            <label for="username"></label>
+        <div class="col-sm-6 offset-3">
+            <label for="username">Nom d'utilisateur</label>
             <input type="text" name="username" id="username" class="form-control">
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password" class="form-control">
         </div>
     </div>
 </div>
