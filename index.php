c8a4e3fec9939821a5fdc8a6e968cfa215c6aa95
Le Flem Paul <61330908+Smoqqqq@users.noreply.github.com>
2021-06-11 14:16:46 +0200

    bootstrap

--- index.php
+++ index.php
@@ -11,13 +11,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $conf_title ?></title>
     <meta name="keywords" content="<?php echo $conf_keywords ?>">
-    <meta name="theme-color" content="#0056b3">
     <meta name="description" content="<?php echo $conf_description ?>">
-    <link rel="stylesheet" href="ressources/css/bootstrapLight.css">
-    <?php if(strpos($slug, "dmin_") < 1) { ?><link rel="stylesheet" href="ressources/css/global.css"> <?php } ?>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" integrity="sha512-Ez0cGzNzHR1tYAv56860NLspgUGuQw16GiOOp/I2LuTmpSK9xDXlgJz3XN4cnpXWDmkNBKXR/VDMTCnAaEooxA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
