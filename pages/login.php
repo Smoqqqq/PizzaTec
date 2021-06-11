0a03621252fbe248fcf7771ee4524024f420428c
Le Flem Paul <61330908+Smoqqqq@users.noreply.github.com>
2021-06-11 14:20:12 +0200

    jquery + font-awesome

---
+++ pages/login.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 offset-6">
+            <input type="text" name="Nom d'utilisateur" id="Nom d'utilisateur" class="form-control">
+        </div>
+    </div>
+</div>
