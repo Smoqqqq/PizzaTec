87be6af7a2a9b457ccffa8135459b81200c2423c
alolilu <33064126+alolilu@users.noreply.github.com>
2021-06-11 14:23:13 +0200

    test

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,8 +1,9 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-lg-3">
+            <i class="fas fa-bars"></i>
+            <h1>dd</h1>
         </div>
-        
         <div class="col-md-lg-5">
         </div>
         <div class="col-md-lg-4">
