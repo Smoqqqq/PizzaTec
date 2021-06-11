55486965fb453ed76b79c60a11e761003da3303e
Le Flem Paul <61330908+Smoqqqq@users.noreply.github.com>
2021-06-11 14:22:05 +0200

    login route

--- pages/login.php
+++ pages/login.php
@@ -1,7 +1,8 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-6 offset-6">
-            <input type="text" name="Nom d'utilisateur" id="Nom d'utilisateur" class="form-control">
+            <label for="username"></label>
+            <input type="text" name="username" id="username" class="form-control">
         </div>
     </div>
 </div>
