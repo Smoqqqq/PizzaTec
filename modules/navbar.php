b1be9f977c1cb6186ebca363220d8e9e184eebf8
alolilu <33064126+alolilu@users.noreply.github.com>
2021-06-11 14:10:51 +0200

    test

---
+++ modules/navbar.php
@@ -0,0 +1,10 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-lg-3">
+        </div>
+        <div class="col-md-lg-5">
+        </div>
+        <div class="col-md-lg-4">
+        </div>
+    </div>
+</div>
